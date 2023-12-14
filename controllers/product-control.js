5c20c131c5db780d9280bed298e42ee762494801
HimanshuVerma9760 <humnavaverma@gmail.com>
2023-12-14 15:21:48 +0530

    added

--- controllers/product-control.js
+++ controllers/product-control.js
@@ -1,9 +1,7 @@
-const product = [];
-var c = 0;
+const Products = require("../models/product");
 exports.prodsPost = (req, res, next) => {
-  const yourProduct = req.body.item;
-  product.push({ title: yourProduct });
-  console.log(yourProduct);
+  const product = new Products(req.body.item);
+  product.save();
   res.redirect("/");
 };
 exports.prodsGet = (req, res, next) => {
@@ -11,6 +9,6 @@ exports.prodsGet = (req, res, next) => {
 };
 
 exports.shopProdGet = (req, res, next) => {
-  c = c + 1;
-  res.render("my-shop", { prods: product, docTitle: "My-Shop" });
+  const products = Products.fetchAll();
+  res.render("my-shop", { prods: products, docTitle: "My-Shop" });
 };
