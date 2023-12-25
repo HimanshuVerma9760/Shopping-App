5142ed32cce6c37cd428a21a50541abeacc6a79c
HimanshuVerma9760 <humnavaverma@gmail.com>
2023-12-26 00:25:19 +0530

    fixed issues

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,12 +1,53 @@
+const fs = require("fs");
+const path = require("path");
 const Cart = require("../models/Cart-model");
 
+// let fetchCart = false;
+const cartItems = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "..", "/file/Cart.json"))
+);
+// const products = JSON.parse(
+//   fs.readFileSync(path.join(__dirname, "..", "/file/Products.json"))
+// );
+
+// for (let index = 0; index < products.length; index++) {
+//   if (cartItems.find((prods) => prods.id === products[index].id)) {
+//     fetchCart = true;
+//     break;
+//   }
+// }
+// if (!fetchCart) {
+//   fs.writeFileSync(path.join(__dirname, "..", "/file/Cart.json"), []);
+// }
 let myCart = Cart.cart.getCartItem();
+let subTotal = 0;
+let addPrice = true;
+let noOfItems = 0;
+function gettingPrice() {
+  var total = 0;
+  for (let index = 0; index < cartItems.length; index++) {
+    total = total + Number(cartItems[index].price) * cartItems[index].qty;
+    if (Number(cartItems[index].qty) > 1) {
+      noOfItems = Number(cartItems[index].qty) + noOfItems;
+    } else {
+      noOfItems++;
+    }
+  }
+  addPrice = false;
+  return total;
+}
 
 exports.cartController = (req, res, next) => {
   Cart.cart.addProduct(req.params.itemId);
   myCart = Cart.cart.getCartItem();
-  res.render("my-cart", { cartItems: myCart });
+  if (addPrice) {
+    subTotal = gettingPrice();
+  }
+  res.render("my-cart", { cartItems: myCart, subTotal, noOfItems });
 };
 exports.cart = (req, res, next) => {
-  res.render("my-cart", { cartItems: myCart });
+  if (addPrice) {
+    subTotal = gettingPrice();
+  }
+  res.render("my-cart", { cartItems: myCart, subTotal, noOfItems });
 };
--- controllers/show-product.js
+++ controllers/show-product.js
@@ -1,16 +1,12 @@
 const fs = require("fs");
 const path = require("path");
+
 exports.showProduct = (req, res, next) => {
-  const id = req.params.productId;
-  const productDetail = this.findProduct(id);
+  const product = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "..", "/file/Products.json"))
+  );
+  const itemId = req.params.productId;
+  const productDetail = product.find((prod) => prod.id == itemId);
+  console.log(productDetail);
   res.render("product-details", { product: productDetail });
-};
-exports.findProduct = (id) => {
-  const product = fs.readFileSync(path.join(__dirname, "../file/Products.json"));
-  const content = JSON.parse(product);
-  for (let index = 0; index < content.length; index++) {
-    if (content[index].id === id) {
-      return content[index];
-    }
-  }
 };
