f9ed651675ab6fc653b4f560a1db24688cf43ba7
HimanshuVerma9760 <humnavaverma@gmail.com>
2023-12-26 13:39:16 +0530

    added remove items function

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const Cart = require("../models/Cart-model");
+const p = path.join(__dirname, "..", "/file/Cart.json");
 
+const cartItems = JSON.parse(fs.readFileSync(p));
 // let fetchCart = false;
-const cartItems = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "..", "/file/Cart.json"))
-);
 // const products = JSON.parse(
 //   fs.readFileSync(path.join(__dirname, "..", "/file/Products.json"))
 // );
@@ -19,11 +18,11 @@ const cartItems = JSON.parse(
 // if (!fetchCart) {
 //   fs.writeFileSync(path.join(__dirname, "..", "/file/Cart.json"), []);
 // }
-let myCart = Cart.cart.getCartItem();
 let subTotal = 0;
 let addPrice = true;
 let noOfItems = 0;
 function gettingPrice() {
+  // let myCart = Cart.cart.getCartItem();
   var total = 0;
   for (let index = 0; index < cartItems.length; index++) {
     total = total + Number(cartItems[index].price) * cartItems[index].qty;
@@ -39,15 +38,25 @@ function gettingPrice() {
 
 exports.cartController = (req, res, next) => {
   Cart.cart.addProduct(req.params.itemId);
-  myCart = Cart.cart.getCartItem();
+  const myCart = Cart.cart.getCartItem();
   if (addPrice) {
     subTotal = gettingPrice();
   }
   res.render("my-cart", { cartItems: myCart, subTotal, noOfItems });
 };
 exports.cart = (req, res, next) => {
+  let myCart = Cart.cart.getCartItem();
+  if (req.params.itemId) {
+    let product = myCart.filter((prod) => prod.id !== req.params.itemId);
+    fs.writeFileSync(p, JSON.stringify(product));
+    myCart = Cart.cart.getCartItem();
+  }
   if (addPrice) {
     subTotal = gettingPrice();
   }
-  res.render("my-cart", { cartItems: myCart, subTotal, noOfItems });
+  res.render("my-cart", {
+    cartItems: myCart,
+    subTotal,
+    noOfItems,
+  });
 };
--- routes/user/add-to-cart.js
+++ routes/user/add-to-cart.js
@@ -3,7 +3,8 @@ const cartController = require("../../controllers/cart-controller");
 
 const route = express.Router();
 
-route.get("/add-to-cart", cartController.cart)
+route.get("/add-to-cart/:itemId", cartController.cart);
+route.get("/add-to-cart/", cartController.cart);
 route.post("/add-to-cart/:itemId", cartController.cartController);
 
 module.exports = route;
